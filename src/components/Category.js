3077c844cf34a5897cb58680189a42447b85f849
Vishal P <vishalprakaash@gmail.com>
2021-04-02 00:43:08 +0530

    added responsive test in banner

---
+++ src/components/Category.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Category() {
+    return (
+        <div className="category">
+            <div className="category_header"></div>
+            <div className="category_list">
+                <div className="category_card"></div>
+                <div className="category_card"></div>
+                <div className="category_card"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Category
