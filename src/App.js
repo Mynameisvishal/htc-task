76cc74c6be73c0660fd0d60f1c3140e5899d3cdf
Vishal P <vishalprakaash@gmail.com>
2021-04-02 09:36:51 +0530

    added Category component with responsiveness

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import Banner from './components/Banner';
+import Category from './components/Category';
 import Navbar from './components/Navbar';
+import Employee from './components/Employee';
+import EmployeeDetails from './components/EmployeeDetails';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Banner />
+      <Category />
+      <Employee />
+      <EmployeeDetails />
+      <Footer />
     </div>
   );
 }
--- src/components/Category.js
+++ src/components/Category.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React from 'react';
+import beautyProducts from "../images/Beauty.jpg"
+import supplement from "../images/Supplements.jpg"
+import fitness from "../images/Fitness.jpg"
+import './Category.css';
 
 function Category() {
     return (
         <div className="category">
-            <div className="category_header"></div>
+            <div className="category_header">
+                <h3>Category</h3>
+                <p>with great offers</p>    
+            </div>
             <div className="category_list">
-                <div className="category_card"></div>
-                <div className="category_card"></div>
-                <div className="category_card"></div>
+                <div className="category_card">
+                    <img src={beautyProducts} width="100%" alt="products"/>
+                    <div className="category_overlap">
+                        <h4>All Natural Non Toxic</h4>
+                        <h2>Best of Beauty</h2>
+                        <button>SHOP NOW</button>
+                    </div>
+                </div>
+                <div className="category_card">
+                    <img src={supplement} width="100%" alt="supplement"/>
+                    <div className="category_overlap">
+                        <h4>Best Deals for</h4>
+                        <h2>Supplements and Vitamins</h2>
+                        <button>SHOP NOW</button>
+                    </div>
+                </div>
+                <div className="category_card">
+                    <img src={fitness} width="100%" alt="fitness"/>
+                    <div className="category_overlap">
+                        <h4>Everyday savings on</h4>
+                        <h2>Fitness and Nutrition</h2>
+                        <button>SHOP NOW</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
